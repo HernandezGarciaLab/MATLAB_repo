de0f128efa923b1e4e4ef7659ad16b9bcc77cafd
fmrifrey <djfrey@umich.edu>
2022-09-09 11:13:43 -0400

    Added and fixed writenii (want to eventually create an object), shuffled more files around, added ndel to recon

---
+++ filemgmt/readnii.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = readnii(inputArg1,inputArg2)
+%READNII Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
--- recon/recon_v3df.m
+++ recon/recon_v3df.m
@@ -7,6 +7,7 @@ function [t,im,psf] = recon_v3df(varargin)
         'tol',          1e-5, ... % Kspace distance tolerance
         'itrmax',       15, ... % Max number of iterations for IR
         'frames',       'all', ... % Frames to recon
+        'ndel',         0, ... % Gradient sample delay
         'pdorder',      -1, ... % Order of phase detrending poly fit
         'nworkers',     feature('numcores'), ... % Number of workers to use in parpool
         'scaleoutput',  1 ... % Option to scale output to full dynamic range
@@ -82,6 +83,11 @@ function [t,im,psf] = recon_v3df(varargin)
         raw = phasedetrend(raw,navpts,args.pdorder);
     end
     
+    % Correct for gradient sample delay
+    if abs(ndel) > 0
+        raw = circshift(raw,args.ndel,2);
+    end
+    
     % Remove ramp points
     fprintf('\nRemoving %d ramp points from data...', nramp);
     ks([1:nramp info.ndat-nramp:info.ndat],:,:,:) = [];
@@ -152,7 +158,7 @@ function [t,im,psf] = recon_v3df(varargin)
     % Save results that aren't returned to nifti:
     if nargout < 2
         % Save timeseries
-        im2nii('./timeseries.nii', abs(im), ...
+        writenii('./timeseries.nii', abs(im), ...
             dim, fov, info.tr, args.scaleoutput);
         fprintf('\nTimeseries saved to timeseries.nii');
     else
@@ -160,7 +166,7 @@ function [t,im,psf] = recon_v3df(varargin)
     end
     if nargout < 3
         % Save point spread function
-        im2nii('./psf.nii', abs(psf), ...
+        writenii('./psf.nii', abs(psf), ...
             dim, fov, info.tr, args.scaleoutput);
         fprintf('\nPSF saved to psf.nii');
     else
@@ -234,143 +240,5 @@ function Wi = pipedcf(G,itrmax)
         Wi = Wi ./ d;
         
     end
-    
-return;
-
-%% im2nii function definition
-function im2nii(niifile_name,im,dim,fov,tr,doscl)
-    
-    % Open nifti file for writing
-    [niifile,msg_fopen] = fopen(niifile_name,'wb','ieee-le');
-    if ~isempty(msg_fopen), error(msg_fopen); end
-
-    % Determine scaling factors for saving full dynamic range
-    if doscl
-        DACFactor = 2^15-1;
-        y = im;
-        y_min = min(y,[],'all'); y_max = max(y,[],'all');
-        m = 2*DACFactor/(y_max - y_min);
-        x = m*y - (m*y_min + DACFactor);
-        scl_inter = (m*y_min + DACFactor)/m;
-        scl_slope = 1/m;
-        im = x;
-    else
-        scl_inter = 0;
-        scl_slope = 0;
-    end
-    
-    % Define header
-    h = struct (...
-        ... % Relevant header info:
-        'sizeof_hdr',   348, ...
-        'dim',          [4, dim, size(im,4), 0, 0, 0], ...
-        'pixdim',       [4, fov./dim, tr, 0, 0, 0], ...
-        'datatype',     4, ...
-        'bitpix',       16, ...
-        'scl_inter',    scl_inter, ...
-        'scl_slope',    scl_slope, ...
-        ...
-        ... % Less relevant header info:
-        'data_type',        repmat(' ',10,1), ...
-        'db_name',          repmat(' ',18,1), ...
-        'extents',          0, ...
-        'session_error',    1, ...
-        'regular',          'r', ...
-        'dim_info',         ' ', ...
-        'intent_p1',        0, ...
-        'intent_p2',        0, ...
-        'intent_p3',        0, ...
-        'intent_code',      0, ...
-        'slice_start',      0, ...
-        'vox_offset',       352, ...
-        'slice_end',        0, ...
-        'slice_code',       ' ', ...
-        'xyzt_units',       ' ', ...
-        'cal_max',          0, ...
-        'cal_min',          0, ...
-        'slice_duration',   0, ...
-        'toffset',          0, ...
-        'glmax',            0, ...
-        'glmin',            0, ...
-        'descrip',          repmat(' ',80,1), ...
-        'aux_file',         repmat(' ',24,1), ...
-        'qform_code',       0, ...
-        'sform_code',       0, ...
-        'quatern_b',        0, ...
-        'quatern_c',        0, ...
-        'quatern_d',        0, ...
-        'qoffset_x',        0, ...
-        'qoffset_y',        0, ...
-        'qoffset_z',        0, ...
-        'srow_x',           zeros(1,4), ...
-        'srow_y',           zeros(1,4), ...
-        'srow_z',           zeros(1,4), ...
-        'intent_name',      repmat(' ',16,1), ...
-        'magic',            repmat(' ',1,4), ...
-        'originator',       zeros(1,4), ...
-        'esize',            0, ...
-        'ecode',            0, ...
-        'edata',            ' ' ...
-    );
-    
-    % Write header info
-    fwrite(niifile, h.sizeof_hdr,       'int32');
-    fwrite(niifile, h.data_type,        'ubit8');
-    fwrite(niifile, h.db_name,          'ubit8');
-    fwrite(niifile, h.extents,          'int32');
-    fwrite(niifile, h.session_error,    'int16');
-    fwrite(niifile, h.regular,          'ubit8');
-    fwrite(niifile, h. dim_info,        'ubit8');
-    fwrite(niifile, h.dim,              'int16');
-    fwrite(niifile, h.intent_p1,        'float32');
-    fwrite(niifile, h.intent_p2,        'float32');
-    fwrite(niifile, h.intent_p3,        'float32');
-    fwrite(niifile, h.intent_code,      'int16');
-    fwrite(niifile, h.datatype,         'int16');
-    fwrite(niifile, h.bitpix,           'int16');
-    fwrite(niifile, h.slice_start,      'int16');
-    fwrite(niifile, h.pixdim,           'float32');
-    fwrite(niifile, h.vox_offset,       'float32');
-    fwrite(niifile, h.scl_slope,        'float32');
-    fwrite(niifile, h.scl_inter,        'float32');
-    fwrite(niifile, h.slice_end,        'int16');
-    fwrite(niifile, h.slice_code,       'ubit8');
-    fwrite(niifile, h.xyzt_units,       'ubit8');
-    fwrite(niifile, h.cal_max,          'float32');
-    fwrite(niifile, h.cal_min,          'float32'); 
-    fwrite(niifile, h.slice_duration,   'float32');
-    fwrite(niifile, h.toffset,          'float32');
-    fwrite(niifile, h.glmax,            'int32');
-    fwrite(niifile, h.glmin,            'int32');
-    fwrite(niifile, h.descrip,          'ubit8');
-    fwrite(niifile, h.aux_file,         'ubit8');
-    fwrite(niifile, h.qform_code,       'int16');
-    fwrite(niifile, h.sform_code,       'int16');
-    fwrite(niifile, h.quatern_b,        'float32');
-    fwrite(niifile, h.quatern_c,        'float32');
-    fwrite(niifile, h.quatern_d,        'float32');
-    fwrite(niifile, h.qoffset_x,        'float32');
-    fwrite(niifile, h.qoffset_y,        'float32');
-    fwrite(niifile, h.qoffset_z,        'float32');
-    fwrite(niifile, h.srow_x,           'float32');
-    fwrite(niifile, h.srow_y,           'float32');
-    fwrite(niifile, h.srow_z,           'float32');
-    fwrite(niifile, h.intent_name,      'ubit8');
-    fwrite(niifile, h.magic,            'ubit8');
-    if length(h.magic)==3
-        fwrite(niifile, 0,              'ubit8');
-    end
-    fwrite(niifile, 0.0,                'float32');
-    fwrite(niifile, repmat(' ',1,13),   'ubit8');
-    
-    % Write data
-    fseek(niifile, h.vox_offset, 'bof');
-    if h.dim(5) >1
-        fwrite(niifile, im(:)', 'short');
-    else
-        fwrite(niifile, im(:), 'short');
-    end
-    
-    fclose(niifile);
     
 return;
